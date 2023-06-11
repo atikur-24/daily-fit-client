bdd302de52d8093788f656565edc592acf78bd1b
Atikur <atikurrahman7223@gmail.com>
2023-06-11 08:04:56 +0600

    added navbar

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            gxhgd
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from '/white-1.svg'
+
+const Navbar = () => {
+    const navItems = <>
+    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/'>Home</NavLink></li>
+    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/classes'>Classes</NavLink></li>
+    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/instructors'>Instructors</NavLink></li>
+    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/dashboard'>Dashboard </NavLink></li>
+    <li><Link to='login'>Login</Link></li>
+    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+      <div className="w-10 rounded-full">
+        <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" />
+      </div>
+    </label>
+    </>
+  return (
+    <div className="navbar bg-base-200">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            { navItems }
+          </ul>
+        </div>
+          <img className="h-10" src={logo} alt="logo" />
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+            { navItems }
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
