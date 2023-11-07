14c359d08b10bf4d528a4ba41386b8cf9e0502f8
Atikur <atikurrahman7223@gmail.com>
2023-11-07 23:46:59 +0600

    fixed expert name

--- src/pages/Home/PopularInstructors/PopularInstructorsCard.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructorsCard.jsx
@@ -1,15 +1,15 @@
 const PopularInstructorsCard = ({ instructor }) => {
-    const { name, img } = instructor;
+  const { name, img } = instructor;
 
-    return (
-        <div>
-            <img className="h-[320px] w-full" src={img} alt="trainer" />
-            <div className="bg-[#565656] text-xl text-white py-5 px-3 space-y-2">
-                <p className="text-xl">{name}</p>
-                <p className="text-gray-300 text-[14px] tracking-wide border-b-2 border-gray-400">Gym Expert</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <img className="h-[320px] w-full" src={img} alt="trainer" />
+      <div className="bg-[#565656] text-xl text-white py-5 px-3 space-y-2">
+        <p className="text-sm lg:text-lg whitespace-nowrap">{name}</p>
+        <p className="text-gray-300 text-[14px] tracking-wide border-b-2 border-gray-400">Gym Expert</p>
+      </div>
+    </div>
+  );
 };
 
 export default PopularInstructorsCard;
