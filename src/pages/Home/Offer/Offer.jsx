dfdf28f15756bb7a7c6ff894648445b0c1f67d0d
Atikur <atikurrahman7223@gmail.com>
2023-11-07 23:57:14 +0600

    added offer section

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -1,12 +1,18 @@
-import OfferImg from '../../../assets/banner/dumbbells.jpg'
+import OfferImg from "../../../assets/banner/dumbbells.jpg";
 
 const Offer = () => {
-    return (
-        <section className='relative'>
-            <img className='lg:h-[600px] h-[400px] w-full' src={OfferImg} alt="banner" />
-            <h2 className='text-white text-3xl lg:text-5xl absolute top-1/2 w-full traking- font-medium lg:font-bold text-center'>FINAL CALL, UP TO 70% OFF SALE. <br /> LET’S GET MOVING</h2>
-        </section>
-    );
+  return (
+    <section className="relative">
+      <img className="lg:h-[600px] h-[400px] w-full" src={OfferImg} alt="banner" />
+      <div className="bg-[#f34e3a] absolute top-1/2 text-center">
+        <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">FINAL CALL, UP TO 70% OFF SALE.</h4>
+        <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">LET’S GET MOVING</h4>
+      </div>
+      {/* <h2 className=" absolute top-1/2 font-medium lg:font-bold text-center">
+         <br /> 
+      </h2> */}
+    </section>
+  );
 };
 
 export default Offer;
