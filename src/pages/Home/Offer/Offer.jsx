ae71ccc011680f6665ab14ed0b4e15701f97934e
Atikur <atikurrahman7223@gmail.com>
2023-11-08 00:03:20 +0600

    final call up desing

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -4,13 +4,12 @@ const Offer = () => {
   return (
     <section className="relative">
       <img className="lg:h-[600px] h-[400px] w-full" src={OfferImg} alt="banner" />
-      <div className="bg-[#f34e3a] absolute top-1/2 text-center">
-        <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">FINAL CALL, UP TO 70% OFF SALE.</h4>
-        <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">LET’S GET MOVING</h4>
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-[#f34e3a] bg-opacity-70 p-4 lg:p-8 rounded-md tracking-wide absolute top-1/3 text-center space-y-2 lg:space-y-6">
+          <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">FINAL CALL, UP TO 70% OFF SALE.</h4>
+          <h4 className="text-white text-2xl lg:text-4xl font-medium lg:font-semibold">LET’S GET MOVING</h4>
+        </div>
       </div>
-      {/* <h2 className=" absolute top-1/2 font-medium lg:font-bold text-center">
-         <br /> 
-      </h2> */}
     </section>
   );
 };
