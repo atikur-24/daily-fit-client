908660ac188702b1ed1f9c94c3e2ef3fd84cc881
atikur-24 <atikurrahman7223@gmail.com>
2024-01-24 23:25:22 +0600

    added some routes in navbar

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,96 +1,133 @@
-import { Link, NavLink } from "react-router-dom";
-import logo from '/white-1.svg'
-import useAuth from "../../hooks/useAuth";
-import useAdmin from "../../hooks/useAdmin";
-import useInstructor from "../../hooks/useInstructor";
 import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import useAdmin from "../../hooks/useAdmin";
+import useAuth from "../../hooks/useAuth";
+import useInstructor from "../../hooks/useInstructor";
+import logo from "/white-1.svg";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
-  const [ isAdmin ] = useAdmin();
-  const [ isInstructor ] = useInstructor();
-  const [ theme, setTheme ] = useState( localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
 
-  const handleThemeToggle = e => {
-    if(e.target.checked) {
-      setTheme('dark');
+  const handleThemeToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
     } else {
-      setTheme('light')
+      setTheme("light");
     }
-  }
+  };
 
-  useEffect( () => {
-    localStorage.setItem('theme', theme);
-    const localTheme = localStorage.getItem('theme');
-    document.querySelector('html').setAttribute('data-theme', localTheme)
-  }, [theme])
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
 
-  let dashboardLink = '/dashboard/studentHome';
+  let dashboardLink = "/dashboard/studentHome";
 
   if (isAdmin) {
-    dashboardLink = '/dashboard/adminHome';
+    dashboardLink = "/dashboard/adminHome";
   } else if (isInstructor) {
-    dashboardLink = '/dashboard/instructorHome';
+    dashboardLink = "/dashboard/instructorHome";
   }
 
   const handleLogout = () => {
-    logout().then(()=>{}).catch(error=>console.error(error))
-  }
+    logout()
+      .then(() => {})
+      .catch((error) => console.error(error));
+  };
 
-    const navItems = <>
-    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/'>Home</NavLink></li>
-    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/classes'>Classes</NavLink></li>
-    <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to='/instructors'>Instructors</NavLink></li>
-    {user && <li><NavLink className={({ isActive }) => (isActive ? "active" : "default")} to={dashboardLink}>Dashboard </NavLink></li>}
+  const navItems = (
+    <>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/classes">
+          Classes
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/instructors">
+          Trainers
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/services">
+          Services
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/aboutUs">
+          About Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to="/contactUs">
+          Contact
+        </NavLink>
+      </li>
+      {user && (
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? "active" : "default")} to={dashboardLink}>
+            Dashboard{" "}
+          </NavLink>
+        </li>
+      )}
     </>
+  );
   return (
     <div className="navbar bg-slate-500 lg:pt-4 lg:pl-20">
       <div className="navbar-start z-10">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-gray-500 rounded-box w-52"
-          >
-            { navItems }
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-gray-500 rounded-box w-52">
+            {navItems}
           </ul>
         </div>
+        <Link to="/">
           <img className="h-10 lg:h-14 mr-5 lg:mr-20" src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-            { navItems }
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
       </div>
       <div className="navbar-end lg:mr-28">
         <div className="mr-10">
           <label onChange={handleThemeToggle} className="swap swap-rotate pt-2">
-              <input type="checkbox" />
-              <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-              <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>           
-            </label>
+            <input type="checkbox" />
+            <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+            </svg>
+            <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            </svg>
+          </label>
         </div>
-        { user ? <button onClick={handleLogout} className="btn btn-sm text-black">Logout</button> : <Link to='/login' className="btn btn-sm text-black">Login</Link>}
-        { user && <label tabIndex={0} className="btn btn-ghost btn-circle avatar ml-3">
-          <div className="w-10 rounded-full">
-            <img title={user?.displayName} src={user?.photoURL} />
-          </div>
-        </label>}
+        {user ? (
+          <button onClick={handleLogout} className="btn btn-sm text-black">
+            Logout
+          </button>
+        ) : (
+          <Link to="/login" className="btn btn-sm text-black">
+            Login
+          </Link>
+        )}
+        {user && (
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar ml-3">
+            <div className="w-10 rounded-full">
+              <img title={user?.displayName} src={user?.photoURL} />
+            </div>
+          </label>
+        )}
       </div>
     </div>
   );
